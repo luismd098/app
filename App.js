a51c8b5769db83998064562a3ad770378a546a64
luismd098 <77755434+luismd098@users.noreply.github.com>
2021-01-21 13:43:01 -0600

    Sustituir este archivo

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppStack from './app/navigator/stack';
+class App extends React.Component {
+  render() {
+    return (
+      <AppStack />
+    );
+  }
+}
+export default App;
+
+
